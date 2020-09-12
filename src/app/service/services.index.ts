bd6b8ba120a447122c692474aef3eeed5ea0ec0a
kbo99 <kbo99.gio@icloud.com>
2020-09-11 20:19:15 -0500

    Delete

--- src/app/service/services.index.ts
+++
@@ -1,5 +0,0 @@
-export { RoleGuardServiceGuard } from './guards/role-guard-service.guard';
-export { LoginGuardGuard } from './guards/login-guard.guard';
-export { UsuarioService } from './Usuario/Usuario.service';
-export { AuthService }   from './AuthService'
-export { MenuService }  from './menu/menu.service'
